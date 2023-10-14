613d240e0de850c80ee457e23151bdbd77246341
Christopher Roldan Sanchez <rolchrissa@gmail.com>
2023-10-14 17:31:35 -0300

    valide envs
    Ensure required environment variables are set
    
    The code now checks if the necessary environment variables are set. If any of them are missing, an error message is displayed and the script exits with a status code of 1.
    
    Configure AWS credentials using aws-cli
    
    AWS credentials are now configured using the aws-cli tool. The access key ID, secret access key, and default region are set based on either the original environment variables or ones with an "INPUT_" prefix.
    
    Run commands to set up New Relic Lambda configurations
    
    The script now runs commands to install New Relic Lambda integrations, layers, and subscriptions. The necessary parameters such as the New Relic account ID and API key, as well as the function name, are passed to these commands.

--- script.sh
+++ script.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-# Ensure environment variables are defined
-if [ -z "$NR_ACCOUNT_ID" ] || [ -z "$NR_API_KEY" ] || [ -z "$FUNCTION_NAME" ]; then
-    echo "Error: Environment variables NR_ACCOUNT_ID, NR_API_KEY, and FUNCTION_NAME must be set."
+# Check if original environment variables are set; otherwise, use ones with INPUT_ prefix
+AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-$INPUT_AWS_ACCESS_KEY_ID}"
+AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-$INPUT_AWS_SECRET_ACCESS_KEY}"
+AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-$INPUT_AWS_DEFAULT_REGION}"
+NR_ACCOUNT_ID="${NR_ACCOUNT_ID:-$INPUT_NR_ACCOUNT_ID}"
+NR_API_KEY="${NR_API_KEY:-$INPUT_NR_API_KEY}"
+FUNCTION_NAME="${FUNCTION_NAME:-$INPUT_FUNCTION_NAME}"
+
+# Ensure required environment variables are set
+if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_DEFAULT_REGION" ] || [ -z "$NR_ACCOUNT_ID" ] || [ -z "$NR_API_KEY" ] || [ -z "$FUNCTION_NAME" ]; then
+    echo "Error: Necessary environment variables are not set."
     exit 1
 fi
 
-# Run the command to install New Relic Lambda integrations
+# Configure AWS credentials using aws-cli
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
+
+# Run commands to set up New Relic Lambda configurations
 newrelic-lambda integrations install --nr-account-id $NR_ACCOUNT_ID --nr-api-key $NR_API_KEY
-
-# Run the command to install New Relic Lambda layers
 newrelic-lambda layers install -f $FUNCTION_NAME --nr-account-id $NR_ACCOUNT_ID --disable-extension
-
-# Run the command to install New Relic Lambda subscriptions
 newrelic-lambda subscriptions install -f $FUNCTION_NAME
